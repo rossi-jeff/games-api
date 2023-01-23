49f1deb32328dc3eecd06434fe9efec62029ed0d
Jeff Rossi <jrossi@intouchgps.com>
2023-01-23 13:58:33 -0500

    add jwt and login

--- src/User/queries.ts
+++ src/User/queries.ts
@@ -1,6 +1,18 @@
-import { QueryResolvers, QueryUserArgs } from "../../generated/graphql";
-import { getUser } from "./db/get-user";
+import {
+	QueryResolvers,
+	QueryUserArgs,
+	QueryUserLoginArgs,
+} from '../../generated/graphql'
+import { getUser } from './db/get-user'
+import { loginUser } from './db/login-user'
 
-export const user: QueryResolvers["user"] = async (_, args: QueryUserArgs) => {
-  return await getUser(args);
-};
+export const user: QueryResolvers['user'] = async (_, args: QueryUserArgs) => {
+	return await getUser(args)
+}
+
+export const userLogin: QueryResolvers['userLogin'] = async (
+	_,
+	args: QueryUserLoginArgs
+) => {
+	return await loginUser(args)
+}
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,87 +1,88 @@
-import { Resolvers } from "../generated/graphql";
-import { hangManCreate, hangManGuess } from "./HangMan/mutations";
-import { hangMen, hangMan } from "./HangMan/queries";
-import { HangMan } from "./HangMan/hangman";
-import { userCreate, userUpdate, userChangePassWord } from "./User/mutations";
-import { user } from "./User/queries";
+import { Resolvers } from '../generated/graphql'
+import { hangManCreate, hangManGuess } from './HangMan/mutations'
+import { hangMen, hangMan } from './HangMan/queries'
+import { HangMan } from './HangMan/hangman'
+import { userUpdate, userChangePassWord, userRegister } from './User/mutations'
+import { user, userLogin } from './User/queries'
 import {
-  word,
-  wordHints,
-  wordRandom,
-  wordRate,
-  wordStats,
-} from "./Word/queries";
-import { guessWords, guessWord } from "./GuessWord/queries";
-import { guessWordCreate, guessWordGuess } from "./GuessWord/mutations";
-import { GuessWord } from "./GuessWord/guess-word";
-import { GuessWordGuess } from "./GuessWord/guess-word-guess";
-import { codeBreakerCreate, codeBreakerGuess } from "./CodeBreaker/mutations";
-import { CodeBreaker } from "./CodeBreaker/code-breaker";
-import { CodeBreakerGuess } from "./CodeBreaker/code-breaker-guess";
+	word,
+	wordHints,
+	wordRandom,
+	wordRate,
+	wordStats,
+} from './Word/queries'
+import { guessWords, guessWord } from './GuessWord/queries'
+import { guessWordCreate, guessWordGuess } from './GuessWord/mutations'
+import { GuessWord } from './GuessWord/guess-word'
+import { GuessWordGuess } from './GuessWord/guess-word-guess'
+import { codeBreakerCreate, codeBreakerGuess } from './CodeBreaker/mutations'
+import { CodeBreaker } from './CodeBreaker/code-breaker'
+import { CodeBreakerGuess } from './CodeBreaker/code-breaker-guess'
 import {
-  codeBreaker,
-  codeBreakers,
-  codeBreakerSolution,
-} from "./CodeBreaker/queries";
-import { seaBattles, seaBattle } from "./SeaBattle/queries";
+	codeBreaker,
+	codeBreakers,
+	codeBreakerSolution,
+} from './CodeBreaker/queries'
+import { seaBattles, seaBattle } from './SeaBattle/queries'
 import {
-  seaBattleCreate,
-  seaBattleShip,
-  seaBattleTurn,
-} from "./SeaBattle/mutations";
-import { SeaBattle } from "./SeaBattle/sea-battle";
-import { SeaBattleShip } from "./SeaBattle/sea-battle-ship";
-import { SeaBattleTurn } from "./SeaBattle/sea-battle-turn";
-import { yachts, yacht } from "./Yacht/queries";
-import { yachtCreate, yachtRoll, yachtScoreTurn } from "./Yacht/mutations";
-import { Yacht } from "./Yacht/yacht";
-import { YachtTurn } from "./Yacht/yacht-turn";
+	seaBattleCreate,
+	seaBattleShip,
+	seaBattleTurn,
+} from './SeaBattle/mutations'
+import { SeaBattle } from './SeaBattle/sea-battle'
+import { SeaBattleShip } from './SeaBattle/sea-battle-ship'
+import { SeaBattleTurn } from './SeaBattle/sea-battle-turn'
+import { yachts, yacht } from './Yacht/queries'
+import { yachtCreate, yachtRoll, yachtScoreTurn } from './Yacht/mutations'
+import { Yacht } from './Yacht/yacht'
+import { YachtTurn } from './Yacht/yacht-turn'
 
 export const resolvers: Resolvers = {
-  Query: {
-    codeBreakers,
-    codeBreaker,
-    codeBreakerSolution,
-    guessWords,
-    guessWord,
-    hangMen,
-    hangMan,
-    seaBattles,
-    seaBattle,
-    user,
-    word,
-    wordHints,
-    wordRandom,
-    wordStats,
-    wordRate,
-    yachts,
-    yacht,
-  },
-  Mutation: {
-    codeBreakerCreate,
-    codeBreakerGuess,
-    guessWordCreate,
-    guessWordGuess,
-    hangManCreate,
-    hangManGuess,
-    seaBattleCreate,
-    seaBattleShip,
-    seaBattleTurn,
-    userCreate,
-    userUpdate,
-    userChangePassWord,
-    yachtCreate,
-    yachtRoll,
-    yachtScoreTurn,
-  },
-  CodeBreaker,
-  CodeBreakerGuess,
-  GuessWord,
-  GuessWordGuess,
-  HangMan,
-  SeaBattle,
-  SeaBattleShip,
-  SeaBattleTurn,
-  Yacht,
-  YachtTurn,
-};
+	Query: {
+		codeBreakers,
+		codeBreaker,
+		codeBreakerSolution,
+		guessWords,
+		guessWord,
+		hangMen,
+		hangMan,
+		seaBattles,
+		seaBattle,
+		user,
+		userLogin,
+		word,
+		wordHints,
+		wordRandom,
+		wordStats,
+		wordRate,
+		yachts,
+		yacht,
+	},
+	Mutation: {
+		codeBreakerCreate,
+		codeBreakerGuess,
+		guessWordCreate,
+		guessWordGuess,
+		hangManCreate,
+		hangManGuess,
+		seaBattleCreate,
+		seaBattleShip,
+		seaBattleTurn,
+		userRegister,
+		userUpdate,
+		userChangePassWord,
+		yachtCreate,
+		yachtRoll,
+		yachtScoreTurn,
+	},
+	CodeBreaker,
+	CodeBreakerGuess,
+	GuessWord,
+	GuessWordGuess,
+	HangMan,
+	SeaBattle,
+	SeaBattleShip,
+	SeaBattleTurn,
+	Yacht,
+	YachtTurn,
+}
